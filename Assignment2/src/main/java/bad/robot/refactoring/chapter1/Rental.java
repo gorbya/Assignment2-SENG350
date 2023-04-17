f6dd3fbdebbcfde083052ec66fd95dbd4dae16ef
Alex Gorby <agorby@aggtool.com>
2023-04-17 15:39:08 -0400

    Refactored

--- Assignment2/src/main/java/bad/robot/refactoring/chapter1/Rental.java
+++ Assignment2/src/main/java/bad/robot/refactoring/chapter1/Rental.java
@@ -21,4 +21,32 @@ public class Rental {
         return daysRented;
     }
 
+	double getChargeForRental() {
+		double amount = 0;
+		switch (getMovie().getPriceCode()) {
+		    case Movie.REGULAR:
+		        amount += 2;
+		        if (getDaysRented() > 2)
+		            amount += (getDaysRented() - 2) * 1.5;
+		        break;
+		    case Movie.NEW_RELEASE:
+		        amount += getDaysRented() * 3;
+		        break;
+		    case Movie.CHILDREN:
+		        amount += 1.5;
+		        if (getDaysRented() > 3)
+		            amount += (getDaysRented() - 3) * 1.5;
+		        break;
+		}
+		return amount;
+	}
+
+	int calculateFrequentRenterPoints() {
+		// add frequent renter points
+		// add bonus for a two day new release rental
+		if (getMovie().getPriceCode() == Movie.NEW_RELEASE && getDaysRented() > 1)
+		    return 2;
+		return 1;
+	}
+
 }
